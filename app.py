c976effcd6df489af910f0f7a77a681073583762
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-11-22 19:45:08 +0100

    feat: use FastAPI #2

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import csv
+import json
 import os
 import shutil
 import sys
+from typing import List
 
 import torch
-from flask import Flask, jsonify, request
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
@@ -37,7 +42,17 @@ from model_preparator import ModelPreparator
 from label_encoder import LabelEncoder
 from cnn_classifier import CnnClassifier
 
-app = Flask(__name__)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200",
+        "https://bike-path-quality.web.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Number of classes
 num_classes = LabelEncoder().num_classes()
@@ -46,75 +61,115 @@ slice_width = 500
 measurement_speed_limit = 5.0
 
 
-@app.route('/predict/cnn', methods=['POST'])
-def predict_cnn():
-    if request.method == 'POST':
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        # Determine number of linear channels based on slice width
-        linear_channels = ModelPreparator().get_linear_channels(slice_width)
-
-        # Define classifier
-        classifier = CnnClassifier(
-            input_channels=1,
-            num_classes=num_classes,
-            linear_channels=linear_channels
-        ).to(device)
-
-        # Load model
-        model_version = "latest"
-        classifier.load_state_dict(torch.load(
-            os.path.join(script_path, "results", "results", "cnn", model_version, "04-modelling", "model.pickle"),
-            map_location=torch.device(device)
-        ))
-        classifier.eval()
-
-        # Make workspace directory
-        workspace_path = os.path.join(script_path, "workspace", "latest")
-        os.makedirs(workspace_path, exist_ok=True)
-
-        # Initialize logger
-        logger = LoggerFacade(workspace_path, console=True, file=True)
-        logger.log_line("Start Prediction")
-
-        payload = request.get_json(force=True)
-        payload_to_json_file(workspace_path, "bike_activity_sample.json", payload)
-        payload_to_csv_file(workspace_path, "bike_activity_sample.csv", payload)
-
-        dataframes = DataLoader().run(
-            logger=logger,
-            data_path=workspace_path
-        )
-
-        try:
-            dataframes = DataFilterer().run(logger=logger, dataframes=dataframes, slice_width=slice_width,
-                                            measurement_speed_limit=measurement_speed_limit,
-                                            keep_unflagged_lab_conditions=True)
-            dataframes = DataTransformer().run(logger=logger, dataframes=dataframes, skip_label_encode_surface_type=True)
-            dataframes = DataNormalizer().run(logger=logger, dataframes=dataframes)
-
-            tensor = ModelPreparator().create_tensor(dataframes=dataframes, device=device)
-
-            outputs = classifier.forward(tensor)
-            _, prediction = outputs.max(1)
-
-            # Delete workspace directory
-            shutil.rmtree(workspace_path)
-
-            return jsonify({"surface_type": DataTransformer().run_reverse(prediction)})
-
-        except Exception as inst:
-            return jsonify({"error": inst.args[0]})
+class BikeActivitySample(BaseModel):
+    bikeActivityUid: str
+    lat: float
+    lon: float
+    speed: float
+    timestamp: int
+    uid: str
 
 
-def payload_to_json_file(results_path, results_file_name, data):
+class BikeActivityMeasurement(BaseModel):
+    accelerometerX: int
+    accelerometerY: int
+    accelerometerZ: int
+    bikeActivitySampleUid: str
+    lat: float
+    lon: float
+    speed: float
+    timestamp: int
+    uid: str
+
+
+class BikeActivitySampleWithMeasurements(BaseModel):
+    bikeActivitySample: BikeActivitySample
+    bikeActivityMeasurements: List[BikeActivityMeasurement] = []
+
+
+class ResultWrapper(BaseModel):
+    surface_type = ""
+    def __init__(self, surface_type: str):
+        super().__init__()
+        self.surface_type = surface_type
+
+
+class ErrorWrapper(BaseModel):
+    error = ""
+    def __init__(self, error: str):
+        super().__init__()
+        self.error = error
+
+
+@app.post('/predict/cnn')
+def predict_cnn(bike_activity_sample_with_measurements: BikeActivitySampleWithMeasurements):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Determine number of linear channels based on slice width
+    linear_channels = ModelPreparator().get_linear_channels(slice_width)
+
+    # Define classifier
+    classifier = CnnClassifier(
+        input_channels=1,
+        num_classes=num_classes,
+        linear_channels=linear_channels
+    ).to(device)
+
+    # Load model
+    model_version = "latest"
+    classifier.load_state_dict(torch.load(
+        os.path.join(script_path, "results", "results", "cnn", model_version, "04-modelling", "model.pickle"),
+        map_location=torch.device(device)
+    ))
+    classifier.eval()
+
+    # Make workspace directory
+    workspace_path = os.path.join(script_path, "workspace", "latest")
+    os.makedirs(workspace_path, exist_ok=True)
+
+    # Initialize logger
+    logger = LoggerFacade(workspace_path, console=True, file=True)
+    logger.log_line("Start Prediction")
+
+    # Serialize sample
+    payload_to_json_file(workspace_path, "bike_activity_sample.json", bike_activity_sample_with_measurements)
+    payload_to_csv_file(workspace_path, "bike_activity_sample.csv", bike_activity_sample_with_measurements)
+
+    dataframes = DataLoader().run(
+        logger=logger,
+        data_path=workspace_path
+    )
+
+    try:
+        dataframes = DataFilterer().run(logger=logger, dataframes=dataframes, slice_width=slice_width,
+                                        measurement_speed_limit=measurement_speed_limit,
+                                        keep_unflagged_lab_conditions=True)
+        dataframes = DataTransformer().run(logger=logger, dataframes=dataframes, skip_label_encode_surface_type=True)
+        dataframes = DataNormalizer().run(logger=logger, dataframes=dataframes)
+
+        tensor = ModelPreparator().create_tensor(dataframes=dataframes, device=device)
+
+        outputs = classifier.forward(tensor)
+        _, prediction = outputs.max(1)
+
+        # Delete workspace directory
+        shutil.rmtree(workspace_path)
+
+        return ResultWrapper(surface_type=DataTransformer().run_reverse(prediction))
+
+    except Exception as inst:
+        return ErrorWrapper(error=inst.args[0])
+
+
+def payload_to_json_file(results_path, results_file_name,
+                         bike_activity_sample_with_measurements: BikeActivitySampleWithMeasurements):
     with open(os.path.join(results_path, results_file_name), "w") as json_file:
-        json_file.write(("%s" % data).replace("\'", "\""))
+        json_content = json.dumps(bike_activity_sample_with_measurements, default=lambda x: x.__dict__)
+        json_file.write("%s" % json_content)
 
 
-def payload_to_csv_file(results_path, results_file_name, data):
-    bike_activity_sample_with_measurements = data
-
+def payload_to_csv_file(results_path, results_file_name,
+                        bike_activity_sample_with_measurements: BikeActivitySampleWithMeasurements):
     bike_activity_uid = 0
     bike_activity_surface_type = "unknown"
     bike_activity_smoothness_type = "unknown"
@@ -143,42 +198,26 @@ def payload_to_csv_file(results_path, results_file_name, data):
             "bike_activity_smoothness_type",
         ])
 
-        bike_activity_sample = bike_activity_sample_with_measurements["bikeActivitySample"]
-        bike_activity_measurements = bike_activity_sample_with_measurements["bikeActivityMeasurements"]
-
-        bike_activity_sample_uid = bike_activity_sample["uid"]
-        bike_activity_sample_surface_type = bike_activity_sample[
-            "surfaceType"] if "surfaceType" in bike_activity_sample else None
-
-        for bike_activity_measurement in bike_activity_measurements:
-            bike_activity_measurement_uid = bike_activity_measurement["uid"]
-            bike_activity_measurement_timestamp = bike_activity_measurement["timestamp"]
-            bike_activity_measurement_lon = bike_activity_measurement["lon"]
-            bike_activity_measurement_lat = bike_activity_measurement["lat"]
-            bike_activity_measurement_speed = bike_activity_measurement["speed"]
-            bike_activity_measurement_accelerometer_x = bike_activity_measurement["accelerometerX"]
-            bike_activity_measurement_accelerometer_y = bike_activity_measurement["accelerometerY"]
-            bike_activity_measurement_accelerometer_z = bike_activity_measurement["accelerometerZ"]
-
+        for bike_activity_measurement in bike_activity_sample_with_measurements.bikeActivityMeasurements:
             csv_writer.writerow([
                 bike_activity_uid,
-                bike_activity_sample_uid,
-                bike_activity_measurement_uid,
-                bike_activity_measurement_timestamp,
-                bike_activity_measurement_lon,
-                bike_activity_measurement_lat,
+                bike_activity_sample_with_measurements.bikeActivitySample.uid,
+                bike_activity_measurement.uid,
+                bike_activity_measurement.timestamp,
+                bike_activity_measurement.lon,
+                bike_activity_measurement.lat,
                 # Input values
-                bike_activity_measurement_speed,
-                bike_activity_measurement_accelerometer_x,
-                bike_activity_measurement_accelerometer_y,
-                bike_activity_measurement_accelerometer_z,
+                bike_activity_measurement.speed,
+                bike_activity_measurement.accelerometerX,
+                bike_activity_measurement.accelerometerY,
+                bike_activity_measurement.accelerometerZ,
                 bike_activity_phone_position,
                 bike_activity_bike_type,
                 # Output values
-                bike_activity_sample_surface_type if bike_activity_sample_surface_type is not None else bike_activity_surface_type,
-                bike_activity_smoothness_type,
+                bike_activity_surface_type,
+                bike_activity_smoothness_type
             ])
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
